1c2227af10900c50b1ba563e6694fd82200f07ba
Joan Mendo <joanmendo05@gmail.com>
2025-06-27 17:36:48 +0200

    aaaa

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/', // ¡Importante! Así los recursos se cargarán correctamente.
+  build: {
+    outDir: 'dist', // Asegúrate de que coincida con el directorio de build.
+  },
   plugins: [react()],
 })
