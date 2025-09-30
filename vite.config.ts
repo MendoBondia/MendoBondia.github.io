3bcc5d33db9d981dbb5cdc73189206e0e8b1e9bf
MendoBondia <joanmendo05@gmail.com>
2025-09-30 21:04:44 +0200

    Arreglando el Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+    base: '/', // ¡Importante! Así los recursos se cargarán correctamente.
+ 
+    plugins: [react()],
 })
